38136446a6ce8605c45bca98ad2a784746ca368c
Freddie <rincofrederico@gmail.com>
2021-12-09 20:38:00 +0000

    chore: add editor settings

--- src/pages/stitches.config.ts
+++ src/pages/stitches.config.ts
@@ -1,15 +1,6 @@
 import { createStitches } from '@stitches/react';
 
-export const {
-  styled,
-  css,
-  globalCss,
-  keyframes,
-  getCssText,
-  theme,
-  createTheme,
-  config,
-} = createStitches({
+export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme, config } = createStitches({
   theme: {
     colors: {
       gray400: 'gainsboro',
