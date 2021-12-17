15e2a0e071cf295c3280b92ee08eb955e0a4efce
BrunoCamposSaltPay <88768996+BrunoCamposSaltPay@users.noreply.github.com>
2021-12-17 15:02:03 +0000

    Feat: Implement first schema

---
+++ src/lib/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+type GlobalPrisma = typeof globalThis & {
+  prisma: PrismaClient;
+};
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!(global as GlobalPrisma).prisma) {
+    (global as GlobalPrisma).prisma = new PrismaClient();
+  }
+  prisma = (global as GlobalPrisma).prisma;
+}
+export default prisma;
