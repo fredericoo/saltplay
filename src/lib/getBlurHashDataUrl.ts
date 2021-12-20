2a376410789e6f23df71555ea5af32a1be9128fd
Freddie <rincofrederico@gmail.com>
2021-12-20 20:38:23 +0000

    fix: remove canvas and blurhash to deploy to vercel (TEST)

--- src/lib/getBlurHashDataUrl.ts
+++ src/lib/getBlurHashDataUrl.ts
@@ -1,15 +1,15 @@
-import { decode } from 'blurhash';
-import { createCanvas } from 'canvas';
+// import { decode } from 'blurhash';
+// import { createCanvas } from 'canvas';
 
 const getBlurHashDataUrl = (blurHash: string, width: number, height: number, punch: number) => {
-  const pixels = decode(blurHash, width, height, punch);
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext('2d');
-  const imageData = ctx.createImageData(width, height);
-  imageData.data.set(pixels);
-  ctx.putImageData(imageData, 0, 0);
-  const blurDataUrl = canvas.toDataURL();
-  return blurDataUrl;
+  // const pixels = decode(blurHash, width, height, punch);
+  // const canvas = createCanvas(width, height);
+  // const ctx = canvas.getContext('2d');
+  // const imageData = ctx.createImageData(width, height);
+  // imageData.data.set(pixels);
+  // ctx.putImageData(imageData, 0, 0);
+  // const blurDataUrl = canvas.toDataURL();
+  return '';
 };
 
 export default getBlurHashDataUrl;
