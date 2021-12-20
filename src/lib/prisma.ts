14f807ed722d6120231257b7df20537e76fcc334
Freddie <rincofrederico@gmail.com>
2021-12-20 21:38:39 +0000

    fix: prisma client creation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client';
-type GlobalPrisma = typeof globalThis & {
-  prisma: PrismaClient;
-};
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!(global as GlobalPrisma).prisma) {
-    (global as GlobalPrisma).prisma = new PrismaClient();
-  }
-  prisma = (global as GlobalPrisma).prisma;
+
+declare global {
+  var prisma: PrismaClient | undefined;
 }
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
 export default prisma;
