7a40b4d3cbf951a29687bf59c41eee25e467ecf8
BrunoCamposSaltPay <brunofdcampos@hotmail.com>
2021-12-31 17:42:27 +0000

    Maths: matchPoints now rounds up, multiplier is now divided by ln 1.25 to account for a faster catch up rate for new joiners

--- src/lib/leaderboard.ts
+++ src/lib/leaderboard.ts
@@ -7,8 +7,8 @@ export const calculateMatchPoints = (p1TotalPoints: number, p2TotalPoints: numbe
   const winnerTotalPoints = pointsDifference > 0 ? p1TotalPoints : p2TotalPoints;
   const loserTotalPoints = pointsDifference > 0 ? p2TotalPoints : p1TotalPoints;
 
-  const multiplier = Math.log10(Math.abs(loserTotalPoints / winnerTotalPoints) + 1) / Math.log10(2);
-  const matchPoints = Math.floor(BASE_MATCH_POINTS * multiplier);
+  const multiplier = Math.log10(Math.abs(loserTotalPoints / winnerTotalPoints) + 1) / Math.log10(1.25);
+  const matchPoints = Math.ceil(BASE_MATCH_POINTS * multiplier);
 
   return matchPoints;
 };
