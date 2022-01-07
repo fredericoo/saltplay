1a7654dbb089567ec6cadd2afadeccf9870dad7d
Freddie <rincofrederico@gmail.com>
2022-01-07 16:31:41 +0000

    feat: compress names to name + first letter or last name

--- src/components/PlayerLink/PlayerLink.tsx
+++ src/components/PlayerLink/PlayerLink.tsx
@@ -8,17 +8,23 @@ type PlayerLinkProps = {
 };
 
 const PlayerLink: ComponentWithAs<'a', FlexProps & PlayerLinkProps> = ({ name, id, ...props }) => {
+  // first name plus first letter of last name
+  const names = name?.split(' ');
+  const displayName = names
+    ? [names[0], names.length > 1 ? names[names.length - 1][0].toUpperCase() : ''].join(' ')
+    : 'Anonymous';
+
   if (!id)
     return (
       <Text as="span" {...props}>
-        {name || 'Anonymous'}
+        {displayName}
       </Text>
     );
 
   return (
     <Link href={`/player/${id}`} passHref>
       <Text as="a" {...props}>
-        {name || 'Anonymous'}
+        {displayName}
       </Text>
     </Link>
   );
