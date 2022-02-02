cee0e251ee8003280f46e33bbe7dcf5438850913
Freddie <rincofrederico@gmail.com>
2022-02-02 18:45:05 +0000

    feat: split into two apis

--- src/components/NewMatchButton/InvitePicker.tsx
+++ src/components/NewMatchButton/InvitePicker.tsx
@@ -1,14 +1,26 @@
-import fetcher from '@/lib/fetcher';
-import { InvitePlayersAPIResponse } from '@/pages/api/players/invite';
+import { SlackMembersAPIResponse } from '@/pages/api/slack';
+import { ExistingSlackUsersAPIResponse } from '@/pages/api/slack/existing';
 import { Box, Text } from '@chakra-ui/react';
+import axios from 'axios';
 import useSWR from 'swr';
 import ErrorBox from '../ErrorBox';
 import PlayerPicker, { PlayerPickerProps } from '../PlayerPicker/PlayerPicker';
 
 type InvitePickerProps = Pick<PlayerPickerProps, 'selectedPlayers' | 'selectedColour' | 'onSelect'>;
 
+const fetchUnregisteredSlackMembers = async (_key: string) => {
+  const allSlack = await axios.get<SlackMembersAPIResponse>('/api/slack').then(res => res.data);
+  const existingSlackUsers = await axios
+    .get<ExistingSlackUsersAPIResponse>('/api/slack/existing')
+    .then(res => res.data);
+
+  if (!allSlack.members) throw new Error('No members found');
+
+  return { ...allSlack, members: allSlack.members.filter(member => !existingSlackUsers.userIds?.includes(member.id)) };
+};
+
 const InvitePicker: React.VFC<InvitePickerProps> = ({ selectedColour, selectedPlayers, onSelect }) => {
-  const { data, error, mutate } = useSWR<InvitePlayersAPIResponse>(`/api/players/invite`, fetcher, {
+  const { data, error, mutate } = useSWR<SlackMembersAPIResponse>(`invite`, fetchUnregisteredSlackMembers, {
     revalidateOnFocus: false,
   });
   if (error || data?.status === 'error')
@@ -20,7 +32,7 @@ const InvitePicker: React.VFC<InvitePickerProps> = ({ selectedColour, selectedPl
         The players you select will be invited to join SaltPlay and may reject the match.
       </Text>
       <PlayerPicker
-        players={data?.users}
+        players={data?.members}
         isAlphabetical
         selectedPlayers={selectedPlayers}
         refetch={mutate}
--- src/pages/api/slack/existing.ts
+++ src/pages/api/slack/existing.ts
@@ -1,37 +1,30 @@
 import prisma from '@/lib/prisma';
-import getAllSlackMembers from '@/lib/slackbot/getAllSlackMembers';
 import { APIResponse } from '@/lib/types/api';
 import { PromiseElement } from '@/lib/types/utils';
 import { NextApiHandler } from 'next';
 import { getServerSession } from 'next-auth';
 import { nextAuthOptions } from '../auth/[...nextauth]';
 
-export type InvitePlayersAPIResponse = APIResponse<{
-  users: PromiseElement<ReturnType<typeof getAllNonExistingSlackMembers>>;
+export type ExistingSlackUsersAPIResponse = APIResponse<{
+  userIds: PromiseElement<ReturnType<typeof getExistingUserIds>>;
 }>;
 
-const getAllNonExistingSlackMembers = async () => {
-  const allUsers = await getAllSlackMembers();
-
+const getExistingUserIds = async () => {
   const existingUsers = await prisma.user.findMany({
     select: { accounts: { where: { provider: 'slack' }, select: { providerAccountId: true } } },
   });
-
-  return allUsers?.filter(
-    user =>
-      !existingUsers?.find(
-        existingUser => !!existingUser?.accounts?.find(account => account?.providerAccountId === user.id)
-      )
-  );
+  const existingUserIds = existingUsers.map(user => user.accounts[0]?.providerAccountId);
+  return existingUserIds;
 };
 
-const invitedUsersHandler: NextApiHandler<InvitePlayersAPIResponse> = async (req, res) => {
+const existingSlackUsersHandler: NextApiHandler<ExistingSlackUsersAPIResponse> = async (req, res) => {
   if (req.method !== 'GET') return res.status(405).json({ status: 'error', message: 'Method not allowed' });
 
   const session = await getServerSession({ req, res }, nextAuthOptions);
   if (!session) return res.status(403).json({ status: 'error', message: 'Not logged in' });
-  const users = await getAllNonExistingSlackMembers();
-  res.status(200).json({ status: 'ok', users });
+
+  const userIds = await getExistingUserIds();
+  res.status(200).json({ status: 'ok', userIds });
 };
 
-export default invitedUsersHandler;
+export default existingSlackUsersHandler;
