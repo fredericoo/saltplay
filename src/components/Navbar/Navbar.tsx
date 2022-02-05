8eb5647dc46eacbad4f5a0c849e76dfd36715078
Freddie <rincofrederico@gmail.com>
2022-02-05 21:55:55 +0000

    style: translucency on nav elements

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,31 @@
 import { HStack, Text } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import FeedbackForm from '../FeedbackForm';
 import Logo from '../Logo/Logo';
 import ModalButton from '../ModalButton';
 import UserMenu from '../UserMenu/UserMenu';
 
 const Navbar: React.VFC = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => setHasScrolled(window.scrollY > 0);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <HStack py={4} px={6} w="100%" zIndex="docked">
+    <HStack
+      py={4}
+      px={6}
+      w="100%"
+      zIndex="docked"
+      position="sticky"
+      top={0}
+      transition="background .6s ease-out"
+      bg={hasScrolled ? 'rgba(237, 242, 247, 0.85)' : undefined}
+      backdropFilter={hasScrolled ? 'saturate(180%) blur(20px)' : undefined}
+    >
       <Link href="/" passHref>
         <HStack as="a">
           <Logo h="2rem" w="2rem" />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ReactElement } from 'react';
 
 class MyDocument extends Document {
@@ -15,10 +15,10 @@ class MyDocument extends Document {
 
           {/* WEB APP */}
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
           <link rel="icon" href="/favicon.png" />
-          <link rel="apple-touch-icon" href="/favicon.png" />
+          <link rel="apple-touch-icon" href="/pwa.png" />
         </Head>
         <body>
           <Main />
