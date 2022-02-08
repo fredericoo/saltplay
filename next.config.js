c292822988be6ede92de2b2eb47bcafebc7784b6
Freddie <rincofrederico@gmail.com>
2022-02-08 21:34:04 +0000

    fix(experimental): remove caching on static resources

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*{/fonts}?',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public,max-age=31536000,immutable',
-          },
-        ],
-      },
-      {
-        source: '/:path*{/splashscreens}?',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public,max-age=31536000,immutable',
-          },
-        ],
-      },
-    ];
-  },
 };
