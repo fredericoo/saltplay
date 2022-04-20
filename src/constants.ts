8df4d64f59c12cad1501c8f1b41434002eeeee69
Freddie <rincofrederico@gmail.com>
2022-04-20 20:37:41 +0100

    fix: removed not implemented flags

--- src/constants.ts
+++ src/constants.ts
@@ -13,4 +13,4 @@ export const BASE_MATCH_POINTS = 10;
 export const MIN_MATCH_POINTS = 1;
 export const MAX_MATCH_POINTS = 100;
 
-export const GAME_FLAGS = { babyBottleIfHumiliated: 1, symmetricalTeams: 2, noTeams: 4 };
+export const GAME_FLAGS = { babyBottleIfHumiliated: 1 };
