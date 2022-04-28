76b64677f218c1d9a7125fb4d35a360c90cfe509
Freddie <rincofrederico@gmail.com>
2022-04-28 09:24:26 +0100

    feat: implement emergency clear all sessions

---
+++ src/lib/api/handlers/sessions/deleteSessionsHandler.ts
@@ -0,0 +1,25 @@
+import { getErrorStack } from '@/lib/prisma';
+import { canViewDashboard } from '@/lib/roles';
+import { APIResponse } from '@/lib/types/api';
+import { nextAuthOptions } from '@/pages/api/auth/[...nextauth]';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { NextApiHandler } from 'next';
+import { getServerSession } from 'next-auth';
+
+export type SessionsDELETEAPIResponse = APIResponse;
+
+const deleteSessionsHandler: NextApiHandler<SessionsDELETEAPIResponse> = async (req, res) => {
+  const session = await getServerSession({ req, res }, nextAuthOptions);
+  const canEdit = canViewDashboard(session?.user.roleId);
+  if (!session || !canEdit) return res.status(401).json({ status: 'error', message: 'Unauthorised' });
+
+  return await prisma?.session
+    .deleteMany()
+    .then(() => res.status(200).json({ status: 'ok' }))
+    .catch((error: PrismaClientKnownRequestError) => {
+      const stack = getErrorStack(error);
+      return res.status(400).json({ status: 'error', stack });
+    });
+};
+
+export default deleteSessionsHandler;
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,9 +1,11 @@
 import Breadcrumbs from '@/components/Breadcrumbs';
+import DeleteButton from '@/components/DeleteButton';
 import SEO from '@/components/SEO';
 import Settings from '@/components/Settings';
 import Admin from '@/layouts/Admin';
 import { PageWithLayout } from '@/layouts/types';
 import { Stack } from '@chakra-ui/react';
+import axios from 'axios';
 
 const AdminPage: PageWithLayout = () => {
   return (
@@ -20,6 +22,19 @@ const AdminPage: PageWithLayout = () => {
         <Settings.Link href="/admin/users" icon="🙋‍♀️">
           Users
         </Settings.Link>
+      </Settings.List>
+      <Settings.List label="Danger zone">
+        <Settings.Item label="Sign out everyone">
+          <DeleteButton
+            onDelete={async () => {
+              await axios.delete(`/api/sessions`);
+              window.location.pathname = '/';
+            }}
+            keyword="confirm"
+          >
+            Clear all sessions
+          </DeleteButton>
+        </Settings.Item>
       </Settings.List>
     </Stack>
   );
