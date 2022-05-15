d1cdff701d951ae3891cd0ce86f93ae397d14f3f
Freddie <rincofrederico@gmail.com>
2022-05-15 15:31:06 +0100

    fix: revalidate URL

--- src/lib/revalidateStaticPages.ts
+++ src/lib/revalidateStaticPages.ts
@@ -1,9 +1,9 @@
 const revalidateStaticPages = async (paths: string[] = ['/']) => {
-  return await fetch(
-    `${process.env.VERCEL_URL}/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}&${paths
-      .map(path => `path=${path}`)
-      .join('&')}`
-  );
+  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+  const url = `${protocol}://${process.env.VERCEL_URL}/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}&${paths
+    .map(path => `path=${path}`)
+    .join('&')}`;
+  return await fetch(url);
 };
 
 export default revalidateStaticPages;
