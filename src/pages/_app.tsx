68fb86c81de9127f15e7ffc0fa72fa56cfe6dbb5
Freddie <rincofrederico@gmail.com>
2022-05-25 23:03:51 +0100

    fix: set ui elements colorScheme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { PageWithLayout } from '@/layouts/types';
 import fetcher from '@/lib/fetcher';
 import { useMixpanel } from '@/lib/mixpanel';
 import Fonts from '@/theme/Fonts';
+import GlobalCSS from '@/theme/GlobalCSS';
 import theme from '@/theme/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
@@ -27,6 +28,7 @@ const App: AppComponent = ({ Component, pageProps: { session, ...pageProps } }) 
       </Head>
       <ChakraProvider theme={theme}>
         <Fonts />
+        <GlobalCSS />
         <SessionProvider session={session}>
           <NextNProgress color="var(--wrkplay-colors-primary-11)" stopDelayMs={0} options={{ showSpinner: false }} />
           <Layout sidebar={pageProps.sidebar}>
---
+++ src/theme/GlobalCSS.tsx
@@ -0,0 +1,16 @@
+import { useColorMode } from '@chakra-ui/react';
+import { Global } from '@emotion/react';
+
+const ColorMode: React.VFC = () => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Global
+      styles={`:root {
+          color-scheme: ${colorMode}
+        }`}
+    />
+  );
+};
+
+export default ColorMode;
