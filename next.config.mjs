8c5c98a8b872ba10fc73d6e94ca13ffb34ebd4b5
Freddie <45042736+fredericoo@users.noreply.github.com>
2022-05-28 22:16:00 +0100

    feat: add security headers (#56)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,68 @@
+// https://securityheaders.com
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src 'none';
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Feature-Policy',
+    value: `accelerometer: 'none';autoplay: 'none';camera: 'none';encrypted-media: 'none';fullscreen=*;geolocation=*;gyroscope: 'none';magnetometer: 'none';microphone: 'none';midi: 'none';sync-xhr: *;usb: 'none';xr-spatial-tracking: 'none'`,
+  },
+  {
+    key: 'Permissions-Policy',
+    value: `accelerometer=();autoplay=();camera=();encrypted-media=();fullscreen=*;geolocation=*;gyroscope=();magnetometer=();microphone=();midi=();sync-xhr=*;usb=();xr-spatial-tracking=()`,
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+];
+
 const nextConfig = {
+  headers: () => [
+    {
+      source: '/',
+      headers: securityHeaders,
+    },
+    {
+      source: '/:path*',
+      headers: securityHeaders,
+    },
+  ],
   reactStrictMode: true,
   swcMinify: true,
   images: {
