a6cd1f00c2cdd19acdb10e61b12faa124dc5fb85
Freddie <rincofrederico@gmail.com>
2022-05-29 13:50:45 +0100

    fix: typo

--- src/theme/Fonts.tsx
+++ src/theme/Fonts.tsx
@@ -4,14 +4,14 @@ const Fonts: React.VFC = () => (
   <Global
     styles={`
       @font-face {
-        font-family: 'GT-Walsheim Pro';
+        font-family: 'GT Walsheim Pro';
         font-style: normal;
         font-weight: normal;
         font-display: swap;
         src: url('/fonts/GT-Walsheim-Pro-Medium.woff2') format('woff2');
       }
       @font-face {
-        font-family: 'GT-Walsheim Pro';
+        font-family: 'GT Walsheim Pro';
         font-style: normal;
         font-weight: bold;
         font-display: swap;
