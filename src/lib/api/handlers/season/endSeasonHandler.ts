381b5992927ff28f5f5b62b1bd088332e42d0d7f
fredericoo <rincofrederico@gmail.com>
2022-07-01 11:24:02 +0100

    feat: conditions on medal giving

--- src/lib/api/handlers/season/endSeasonHandler.ts
+++ src/lib/api/handlers/season/endSeasonHandler.ts
@@ -56,42 +56,45 @@ const endSeasonHandler: NextApiHandler<SeasonEndAPIResponse> = async (req, res) 
     },
   });
 
-  console.log(top10);
+  const noOfPlayers = await prisma.playerScore.count({ where: { seasonid: id } });
+  const noOfMatches = await prisma.match.count({ where: { seasonid: id } });
 
-  const top10Medal = medals.find(medal => medal.image === 'top-10');
-  if (top10Medal) {
-    await Promise.all(top10.map(({ playerid }) => giveMedal(top10Medal.id, playerid)));
-  }
+  if (noOfPlayers >= 5 && noOfMatches >= 15) {
+    const top10Medal = medals.find(medal => medal.image === 'top-10');
+    if (top10Medal && noOfPlayers >= 20 && noOfMatches >= 40) {
+      await Promise.all(top10.map(({ playerid }) => giveMedal(top10Medal.id, playerid)));
+    }
 
-  const firstPlaceMedal = medals.find(medal => medal.image === 'first-place');
-  if (firstPlaceMedal) {
-    const firstPlace = top10[0];
-    await giveMedal(firstPlaceMedal.id, firstPlace.playerid);
-  }
+    const firstPlaceMedal = medals.find(medal => medal.image === 'first-place');
+    if (firstPlaceMedal) {
+      const firstPlace = top10[0];
+      await giveMedal(firstPlaceMedal.id, firstPlace.playerid);
+    }
 
-  const secondPlaceMedal = medals.find(medal => medal.image === 'second-place');
-  if (secondPlaceMedal) {
-    const secondPlace = top10[1];
-    await giveMedal(secondPlaceMedal.id, secondPlace.playerid);
-  }
+    const secondPlaceMedal = medals.find(medal => medal.image === 'second-place');
+    if (secondPlaceMedal) {
+      const secondPlace = top10[1];
+      await giveMedal(secondPlaceMedal.id, secondPlace.playerid);
+    }
 
-  const thirdPlaceMedal = medals.find(medal => medal.image === 'third-place');
-  if (thirdPlaceMedal) {
-    const thirdPlace = top10[2];
-    await giveMedal(thirdPlaceMedal.id, thirdPlace.playerid);
-  }
+    const thirdPlaceMedal = medals.find(medal => medal.image === 'third-place');
+    if (thirdPlaceMedal) {
+      const thirdPlace = top10[2];
+      await giveMedal(thirdPlaceMedal.id, thirdPlace.playerid);
+    }
 
-  const lastPlaceMedal = medals.find(medal => medal.image === 'last-place');
-  if (lastPlaceMedal) {
-    const [lastPlace] = await prisma.season.findUnique({ where: { id } }).scores({
-      take: 1,
-      orderBy: [{ points: 'asc' }, { player: { name: 'desc' } }], // 🤖 TODO: revert leaderboardOrderBy
-      select: {
-        playerid: true,
-      },
-    });
+    const lastPlaceMedal = medals.find(medal => medal.image === 'last-place');
+    if (lastPlaceMedal) {
+      const [lastPlace] = await prisma.season.findUnique({ where: { id } }).scores({
+        take: 1,
+        orderBy: [{ points: 'asc' }, { player: { name: 'desc' } }], // 🤖 TODO: revert leaderboardOrderBy
+        select: {
+          playerid: true,
+        },
+      });
 
-    await giveMedal(lastPlaceMedal.id, lastPlace.playerid);
+      await giveMedal(lastPlaceMedal.id, lastPlace.playerid);
+    }
   }
 
   await prisma.season.update({
